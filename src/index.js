85afa5c1c6fed4f32f21a694ff933f5bdc0c9ff0
Jane Kim <jane.kim0717@gmail.com>
2022-02-09 20:56:08 -0500

    Completed Deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  let form = document.getElementById("create-task-form");
+ 
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    let inputValue = event.target.children[1].value
+
+    newLi = document.createElement("li");
+    newLi.innerText = inputValue
+
+    let myTodos = document.getElementById("tasks");
+    myTodos.appendChild(newLi)
+  })
+
 });
+
+
